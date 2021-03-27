dc25645f5eb0cb6092f0a2ca73bd6f033ded9e9c
Gayane <gayaneghazaryan000@gmail.com>
2021-03-27 12:23:10 +0400

    First Commit

---
+++ Homework1.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        byte a = 123;
+        short b = 1000;
+        int c = 1234432;
+        long d = 2148753648L;
+        float e = 34.564f;
+        double f = 123.456434521;
+        boolean g = true;
+        char h = 'G';
+        String i = "Hi";
+
+        System.out.println("The value of the variable a of type byte: " + a);
+        System.out.println("The value of the variable b of type short: " + b);
+        System.out.println("The value of the variable c of type int: " + c);
+        System.out.println("The value of the variable d of type long: " + d);
+        System.out.println("The value of the variable e of type float: " + e);
+        System.out.println("The value of the variable f of type double: " + f);
+        System.out.println("The value of the variable g of type boolean: " + g);
+        System.out.println("The value of the variable h of type char: " + h);
+        System.out.println("The value of the variable i of type String: " + i);
+        System.out.println();
+
+        c = 14565345;
+        System.out.println("The changed value of c equals to " + c);
+
+        g = false;
+        System.out.println("The changed value of g equals to " + g);
+
+        System.out.println("Hello," + " I " + "am "+ "Gayane.");
+    }
+}
